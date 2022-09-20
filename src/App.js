fbf440b866f013b1fae217e4caae45008e285979
namle <namlh4@vng.com.vn>
2022-09-20 22:09:30 +0700

    chore: replace by native stack

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'react-native-gesture-handler';
 import RootNavigator from './navigations';
 
 export default function App() {
--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {GameScreen, HomeScreen} from '../screens';
 import {ScreenKeys} from './ScreenKeys';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function RootNavigator() {
   return (
