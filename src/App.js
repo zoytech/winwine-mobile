e9f7d959f83ee7e895979cece4a3b6b16b4ed922
Thanh Nam <namnguyenthanhdev@gmail.com>
2022-09-20 22:52:26 +0700

    chore: remove unused code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import 'react';
-=======
 import React from 'react';
->>>>>>> origin/feature/init-project-structure
 import RootNavigator from './navigations';
 
 export default function App() {
