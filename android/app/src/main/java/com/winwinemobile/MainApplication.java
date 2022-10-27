cb1d65b68ee3a988a248868e80c7114440f4f6c8
Thanh Nam <namnguyenthanhdev@gmail.com>
2022-10-27 16:38:19 +0700

    error: add fresco cause crash app

--- android/app/src/main/java/com/winwinemobile/MainApplication.java
+++ android/app/src/main/java/com/winwinemobile/MainApplication.java
@@ -54,7 +54,6 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    Fresco.initialize(this);
     // If you opted-in for the New Architecture, we enable the TurboModule system
     ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
