1bd9cf25bfc7511290c9e2e2c919b97b0426e5ad
Thanh Nam <namnguyenthanhdev@gmail.com>
2022-12-27 17:40:40 +0700

    error: try error boundary but cannot run properly

--- src/screens/errorScreens/ErrorBoundary.js
+++ src/screens/errorScreens/ErrorBoundary.js
@@ -2,6 +2,11 @@ import {Component} from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
 import {FilledButton} from 'src/components';
+import {Color, ColorVariant, Typography} from 'src/themes';
+
+const TITLE = 'Rất tiếc, đã xảy ra lỗi';
+const DESCRIPTION =
+  'Ứng dụng gặp sự cố và không thể tiếp tục. Chúng tôi xin lỗi vì sự bất tiện này! Nhấn nút bên dưới để trở lại màn hình chính. Vui lòng liên hệ với chúng tôi nếu sự cố này vẫn tiếp diễn.';
 
 class ErrorBoundary extends Component {
   state = {
@@ -12,34 +17,43 @@ class ErrorBoundary extends Component {
     return {error: true};
   }
 
-  handleBackToHomeScreen() {
-    alert('navigate');
-  }
+  resetError = () => {
+    this.setState({error: false});
+    console.log('resetError');
+  };
 
   render() {
-    // const onBaseColor = Color.light[ColorVariant.error]?.onBase;
+    const {base: baseColor, onBase: onBaseColor} =
+      Color.light[ColorVariant.background];
+    const {base: errorColor, onBase: onErrorColor} =
+      Color.light[ColorVariant.error];
+    const containerStyle = [styles.container, {backgroundColor: baseColor}];
+    const contentStyle = [{color: onBaseColor}];
     if (this.state.error) {
       return (
         <SafeAreaView>
-          <View style={styles.container}>
-            <View style={styles.content}>
-              <Icon name={'exclamationcircle'} size={60} color={'red'} />
-              <Text style={{fontSize: 32}}>Oops, Something Went Wrong</Text>
-              <Text
-                style={{marginVertical: 10, lineHeight: 23, fontWeight: '500'}}>
-                The app ran into a problem and could not continue. We apologise
-                for any inconvenience this has caused! Press the button below to
-                restart the app and sign back in. Please contact us if this
-                issue persists.
+          <View style={containerStyle}>
+            <Icon
+              name={'exclamationcircleo'}
+              size={40}
+              color={errorColor}
+              style-={styles.iconDisplay}
+            />
+            <View style={styles.title}>
+              <Text style={[contentStyle, Typography.title.large]}>
+                {TITLE}
               </Text>
-              <FilledButton
-                content={'Back to Sign In Screen'}
-                onPress={() => this.handleBackToHomeScreen()}
-                style={{
-                  marginVertical: 15,
-                }}
-              />
             </View>
+            <Text
+              style={[contentStyle, styles.description, Typography.body.large]}
+              numberOfLines={7}>
+              {DESCRIPTION}
+            </Text>
+            <FilledButton
+              content={'Reset lỗi'}
+              onPress={() => this.resetError}
+              style={[contentStyle, styles.button]}
+            />
           </View>
         </SafeAreaView>
       );
@@ -49,6 +63,35 @@ class ErrorBoundary extends Component {
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 100,
+    paddingHorizontal: 24,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    paddingVertical: 20,
+  },
+  description: {
+    paddingVertical: 10,
+    alignContent: 'center',
+    textAlign: 'center',
+  },
+  iconDisplay: {
+    paddingTop: 50,
+    justifyContent: 'center',
+  },
+  button: {
+    width: '90%',
+    aspectRatio: 8,
+    marginVertical: 15,
+  },
+});
 
 export default ErrorBoundary;
+
+/*
+
+ */
